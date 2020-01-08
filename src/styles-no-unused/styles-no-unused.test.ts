8a755a143b743aed5ab3436d1cda37f97ccbad17
Jed Richards <jedrichards@gmail.com>
2020-01-08 13:00:31 +0000

    feat: add styles-no-unused rule (#24)

---
+++ src/styles-no-unused/styles-no-unused.test.ts
@@ -0,0 +1,43 @@
+import { resolve } from 'path'
+import {
+  Config,
+  invokeRule,
+  createDummyConfig,
+} from '@sketch-hq/sketch-lint-core'
+
+import { ruleModule } from '.'
+import { ruleSet } from '..'
+
+const { name } = ruleModule
+
+const config: Config = createDummyConfig({
+  rules: {
+    [name]: { active: true },
+  },
+})
+
+test('Generates violations correctly', async (): Promise<void> => {
+  expect.assertions(1)
+  const violations = await invokeRule(
+    resolve(__dirname, '../../fixtures/styles-no-unused.sketch'),
+    config,
+    ruleSet,
+    ruleModule,
+  )
+  expect(violations.map(violation => violation.message)).toMatchInlineSnapshot(`
+    Array [
+      "Unexpected unused shared style",
+    ]
+  `)
+})
+
+test('Does not generate false negatives', async (): Promise<void> => {
+  expect.assertions(1)
+  const violations = await invokeRule(
+    resolve(__dirname, '../../fixtures/empty.sketch'),
+    config,
+    ruleSet,
+    ruleModule,
+  )
+  expect(violations).toHaveLength(0)
+})
