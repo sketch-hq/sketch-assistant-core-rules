19ace5604f33591582a3ca541d4f59ae81195a63
Jed Richards <jedrichards@gmail.com>
2020-02-06 10:54:26 +0000

    fix: include image fills in images-no-outsized (#48)

--- src/images-no-outsized/__tests__/index.ts
+++ src/images-no-outsized/__tests__/index.ts
@@ -25,7 +25,9 @@ const testRule = async (fixture: string): Promise<LintViolation[]> =>
     ruleModule,
   )
 
-test('No violations correctly used bitmap', async (): Promise<void> => {
+test('No violations when a bitmap is used correctly at least once', async (): Promise<
+  void
+> => {
   expect.assertions(1)
   const violations = await testRule('./multi-use-bitmap.sketch')
   expect(violations.map(violation => violation.message)).toMatchInlineSnapshot(
@@ -38,7 +40,19 @@ test('Finds violations for outsized bitmap use', async (): Promise<void> => {
   const violations = await testRule('./outsized-bitmap.sketch')
   expect(violations.map(violation => violation.message)).toMatchInlineSnapshot(`
     Array [
-      "Unexpected x2 oversized image",
+      "Unexpected oversized image used in image layer, must be no more than 2 times larger than the layer frame's width or height",
+    ]
+  `)
+})
+
+test('Finds violations for outsized bitmap fills in layer styles', async (): Promise<
+  void
+> => {
+  expect.assertions(1)
+  const violations = await testRule('./outsized-bitmap-fill.sketch')
+  expect(violations.map(violation => violation.message)).toMatchInlineSnapshot(`
+    Array [
+      "Unexpected oversized image used in layer style image fill, must be no more than 2 times larger than the layer frame's width or height",
     ]
   `)
 })
